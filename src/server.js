8d39cd0a38f78f843585f4937a5e98d5d2cc9e36
Jon <urbangentryjon@gmail.com>
2022-01-22 17:11:22 +0700

    final template single page

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const fs = require('fs')
+const path = require('path')
+
+app.get('/', function(req,res)
+{
+    const pathToHtmlFile = path.resolve(__dirname, '../dist/index.html')
+    const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8')
+    res.send(contentFromHtmlFile)
+})
+
+app.use('/static', express.static(path.resolve(__dirname, '../dist')))
+
+app.listen(3000, function()
+{
+    console.log('listening on port 3000')
+})
